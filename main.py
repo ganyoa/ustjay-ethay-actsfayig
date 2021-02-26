e8d6f9c091db27b8143eb0fea6fe76dfac9c9b39
Anthony Ganyo <ganyoa@gmail.com>
2021-02-26 12:52:05 -0800

    update to main file

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+scrape and mashup
+scrape random quote from http://unkno.com
+mashup in pig latinizer https://hidden-journey-62459.herokuapp.com/
+
+"""
+
 import os
 
 import requests
@@ -8,7 +15,9 @@ app = Flask(__name__)
 
 
 def get_fact():
-
+    """
+    scrape random fact from unkno website
+    """
     response = requests.get("http://unkno.com")
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -16,10 +25,27 @@ def get_fact():
 
     return facts[0].getText()
 
+def get_pig(fact):
+    """
+    fuction to utilize get_fact() as an arguement (the random fact) and
+    return the url result from the pig latinizer app
+    """
+    payload = {'input_text': fact}
+    response = requests.post(
+                             'https://hidden-journey-62459.herokuapp.com/piglatinize/',
+                              allow_redirects=False,
+                              data=payload)
+
+    return response.headers['Location']
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    """
+    the random fact retrieved from get_fact() becomes the arguement
+    in get_pig(fact) and subsequently the input_text into the pig latinizer
+    """
+    return Response(response=get_pig(get_fact()))
+
 
 
 if __name__ == "__main__":
